80b673ad3e3f8383789551a96de1e22457f29884
dipa <dipaalmadima12@gmail.com>
2021-01-12 09:33:38 +0700

    study kasus

---
+++ Promos/Penawaran.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Promos.Controller;
+using Promos.Model;
+
+namespace Promos
+{
+    /// <summary>
+    /// Interaction logic for Penawaran.xaml
+    /// </summary>
+
+    public partial class Penawaran : Window
+    {
+        PenawaranController Penawarancontroller;
+        OnPenawaranChangedListener listener;
+        public Penawaran()
+        {
+            InitializeComponent();
+
+            Penawarancontroller = new PenawaranController();
+            listPenawaran.ItemsSource = Penawarancontroller.getItems();
+
+            generateContentPenawaran();
+
+        }
+
+        public void SetOnItemSelectedListener(OnPenawaranChangedListener listener)
+        {
+            this.listener = listener;
+        }
+
+        private void generateContentPenawaran()
+        {
+            Item coffeLate = new Item("Coffe Late", 15000);
+            Item greenTea = new Item("Green tea", 10000);
+            Item milkShake = new Item("Milk Shake", 12000);
+            Item alpukatJuice = new Item("Alpukat Juice", 15000);
+            Item lemonSquash = new Item("Lemon Squash", 15000);
+            Item chickenwings = new Item("Chicken wings (8 pcs)", 35000);
+            Item friedRice = new Item("Fried Rice Special", 20000);
+
+            Penawarancontroller.addItem(coffeLate);
+            Penawarancontroller.addItem(greenTea);
+            Penawarancontroller.addItem(milkShake);
+            Penawarancontroller.addItem(alpukatJuice);
+            Penawarancontroller.addItem(lemonSquash);
+            Penawarancontroller.addItem(chickenwings);
+            Penawarancontroller.addItem(friedRice);
+
+            listPenawaran.Items.Refresh();
+        }
+
+        private void listPenawaran_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox listbox = sender as ListBox;
+            Item item = listbox.SelectedItem as Item;
+
+            this.listener.onPenawaranSelected(item);
+        }
+    }
+
+    public interface OnPenawaranChangedListener
+    {
+        void onPenawaranSelected(Item item);
+    }
+}
---
+++ Promos/PilihVoucher.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Promos
+{
+    /// <summary>
+    /// Interaction logic for PilihVoucher.xaml
+    /// </summary>
+    public partial class PilihVoucher : Window
+    {
+        Controller.VoucherController voucherController;
+        OnPilihVoucherChangedListener listener;
+
+        public PilihVoucher()
+        {
+            InitializeComponent();
+
+            voucherController = new Controller.VoucherController();
+            DaftarVoucher.ItemsSource = voucherController.getItems();
+
+            generateListVoucher();
+        }
+
+        private void generateListVoucher()
+        {
+            Model.Voucher seninspesial = new Model.Voucher(title: "Promo Senin Spesial 25%", discInPercent: 25);
+            Model.Voucher penggunabaru = new Model.Voucher(title: "Promo Pengguna Baru 50%", discInPercent: 50);
+            Model.Voucher promoNatal = new Model.Voucher(title: "Promo Natal Potongan 10000", disc: 10000);
+
+            voucherController.addItem(seninspesial);
+            voucherController.addItem(penggunabaru);
+            voucherController.addItem(promoNatal);
+
+            DaftarVoucher.Items.Refresh();
+        }
+
+        public void SetOnItemSelectedListener(OnPilihVoucherChangedListener listener)
+        {
+            this.listener = listener;
+        }
+
+        public interface OnPilihVoucherChangedListener
+        {
+            void OnPilihVoucherChangedListener(Model.Voucher item);
+        }
+
+        private void DaftarVoucherSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox listbox = sender as ListBox;
+            Model.Voucher item = listbox.SelectedItem as Model.Voucher;
+
+            this.listener.OnPilihVoucherChangedListener(item);
+        }
+    }
+}
